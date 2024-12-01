23f9138058783035f40c2832c77604208d24e8c7
25Miki <mariamarchantellopis@gmail.com>
2024-12-01 22:13:17 +0100

    ..

---
+++ who/gatetes.js
@@ -0,0 +1,184 @@
+var gatetes = [
+    {
+      "id": 1,
+      "nombre": "milo",
+      "raza": "siames",
+      "color_pelo": "beige",
+      "color_ojos": "azules",
+      "edad": "joven",
+      "img": "https://www.elespectador.com/resizer/jVPB7E7VQQrCgBr-MLTkIm4Oc7o=/arc-anglerfish-arc2-prod-elespectador/public/VWG5YLUQP5FHFGVU4PQK2QTU3U.jpg"
+    },
+    {
+      "id": 2,
+      "nombre": "luna",
+      "raza": "persa",
+      "color_pelo": "blanco",
+      "color_ojos": "amarillos",
+      "edad": "viejo",
+      "img": "https://cdn0.expertoanimal.com/es/posts/2/0/7/cuidados_del_gato_persa_20702_600.jpg"
+    },
+    {
+      "id": 3,
+      "nombre": "oliver",
+      "raza": "bengali",
+      "color_pelo": "naranja",
+      "color_ojos": "verdes",
+      "edad": "joven",
+      "img": "https://www.dondemiveterinario.com/wp-content/uploads/2021/02/gato-bengali.jpg"
+    },
+    {
+      "id": 4,
+      "nombre": "bella",
+      "raza": "mainecoon",
+      "color_pelo": "gris",
+      "color_ojos": "marrones",
+      "edad": "joven",
+      "img": "https://www.creativefabrica.com/wp-content/uploads/2022/11/02/Big-Grey-Maine-Coon-Cat-44293680-1.png"
+    },
+    {
+      "id": 5,
+      "nombre": "simba",
+      "raza": "siames",
+      "color_pelo": "beige",
+      "color_ojos": "azules",
+      "edad": "bebe",
+      "img": "https://www.superpet.ec/wp-content/uploads/2022/04/siames.jpg"
+    },
+    {
+      "id": 6,
+      "nombre": "chloe",
+      "raza": "persa",
+      "color_pelo": "gris",
+      "color_ojos": "verdes",
+      "edad": "joven",
+      "img": "https://i.pinimg.com/236x/14/9e/a9/149ea9dabb666c5a3adf11793a3d30d6.jpg"
+    },
+    {
+      "id": 7,
+      "nombre": "max",
+      "raza": "europeo",
+      "color_pelo": "naranja",
+      "color_ojos": "azules",
+      "edad": "bebe",
+      "img": "https://img.freepik.com/fotos-premium/animales-compania-gatito-naranja-lindo-bebe-gato-interior_693630-20738.jpg?w=360"
+    },
+    {
+      "id": 8,
+      "nombre": "lily",
+      "raza": "mainecoon",
+      "color_pelo": "marron",
+      "color_ojos": "amarillos",
+      "edad": "viejo",
+      "img": "https://www.elgatofeliz.es/wp-content/uploads/2021/03/maine-coon3.jpg"
+    },
+    {
+      "id": 9,
+      "nombre": "leo",
+      "raza": "europeo",
+      "color_pelo": "gris",
+      "color_ojos": "verdes",
+      "edad": "joven",
+      "img": "https://soydegatos.com/wp-content/uploads/2022/09/gato-europeo-de-pelo-corto.jpg"
+    },
+    {
+      "id": 10,
+      "nombre": "daisy",
+      "raza": "persa",
+      "color_pelo": "negro",
+      "color_ojos": "amarillos",
+      "edad": "bebe",
+      "img": "https://img.freepik.com/fotos-premium/adorable-gato-persa-negro_58409-14454.jpg"
+    },
+    {
+      "id": 11,
+      "nombre": "charlie",
+      "raza": "bengali",
+      "color_pelo": "gris",
+      "color_ojos": "verdes",
+      "edad": "joven",
+      "img": "https://www.tugatobengali.com/wp-content/uploads/2021/09/cepilladogatobengali.jpg"
+    },
+    {
+      "id": 12,
+      "nombre": "sophie",
+      "raza": "mainecoon",
+      "color_pelo": "blanco",
+      "color_ojos": "verdes",
+      "edad": "bebe",
+      "img": "https://gatos.mejoresproductos.es/wp-content/uploads/2022/07/293947416_110648524972601_5140785390678066063_n-1.webp"
+    },
+    {
+      "id": 13,
+      "nombre": "rocky",
+      "raza": "british",
+      "color_pelo": "beige",
+      "color_ojos": "marrones",
+      "edad": "joven",
+      "img": "https://gatobritish.es/wp-content/uploads/2020/04/gato.british.shorthair.cream_-1024x807.jpg"
+    },
+    {
+      "id": 14,
+      "nombre": "molly",
+      "raza": "persa",
+      "color_pelo": "naranja",
+      "color_ojos": "verdes",
+      "edad": "joven",
+      "img": "https://canplanet.com/assets/files/consells/consells-2/_0000_26.jpg"
+    },
+    {
+      "id": 15,
+      "nombre": "oscar",
+      "raza": "angora",
+      "color_pelo": "negro",
+      "color_ojos": "amarillos",
+      "edad": "viejo",
+      "img":"https://live.staticflickr.com/7362/9140665873_9d4dd0ae04_b.jpg"
+    },
+    {
+      "id": 16,
+      "nombre": "luna",
+      "raza": "europea",
+      "color_pelo": "tuxedo",
+      "color_ojos": "verdes",
+      "edad": "bebe",
+      "img": "https://i.pinimg.com/736x/92/9c/7e/929c7ef420838ec9fc4cbd97f455e675.jpg"
+    },
+    {
+      "id": 17,
+      "nombre": "zoe",
+      "raza": "angora",
+      "color_pelo": "beige",
+      "color_ojos": "azules",
+      "edad": "joven",
+      "img":"https://i.pinimg.com/originals/1a/2d/1a/1a2d1a4fce61ecc4630cc2a1f2386286.jpg"
+    },
+    {
+      "id": 18,
+      "nombre": "cleo",
+      "raza": "british",
+      "color_pelo": "gris",
+      "color_ojos": "amarillos",
+      "edad": "viejo",
+      "img":"https://content.elmueble.com/medio/2023/05/31/gato-british_8086d5da_230601112402_1200x630.jpg"
+    },
+    {
+      "id": 19,
+      "nombre": "felix",
+      "raza": "bengali",
+      "color_pelo": "negro",
+      "color_ojos": "azules",
+      "edad": "bebe",
+      "img":"https://abengalcat.com/wp-content/uploads/2017/08/charcoal-bengalcat-3-e1502831369237.jpg"
+    },
+    {
+      "id": 20,
+      "nombre": "willow",
+      "raza": "siames",
+      "color_pelo": "naranja",
+      "color_ojos": "azules",
+      "edad": "viejo",
+      "img": "https://www.bunko.pet/img/2021/05/17/siamxs_red_point_crop1621294029570.jpeg?__scale=w:1200,h:1200,t:2"
+    }
+]
+  
+  

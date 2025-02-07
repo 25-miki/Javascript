02a1da1885131bd2ed611da2c3479337ea3579e0
25Miki <mariamarchantellopis@gmail.com>
2025-02-07 10:51:32 +0100

    ..

--- kanji/script.js
+++ kanji/script.js
@@ -475,7 +475,7 @@ function checkMatch() {
 
 // Restablecer estilo de botones
 function resetButtonStyles() {
-    document.querySelectorAll('.botones').forEach(button => {
+    document.querySelector('.botones').forEach(button => {
         button.style.backgroundColor = '#fff';
     });
 }
